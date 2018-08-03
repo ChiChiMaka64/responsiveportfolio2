27625fb33940e7ede5cee15e7dabe0ef08322896
Lynette Okoroike <lcokoroike@gmail.com>
2018-08-03 13:13:23 -0700

    Turning it into a very active portfolio.

---
+++ javascript/contact.js
@@ -0,0 +1,26 @@
+var config = {
+    apiKey: "AIzaSyCW8jAx_cmOXSyknFunYWyXJLU7nZfdCOI",
+    authDomain: "contact-page-database.firebaseapp.com",
+    databaseURL: "https://contact-page-database.firebaseio.com",
+    projectId: "contact-page-database",
+    storageBucket: "contact-page-database.appspot.com",
+    messagingSenderId: "44308729866"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+  $("button").on("click", function (e) {
+    e.preventDefault()
+    console.log($(this).text());
+    var name=$("#name").val().trim()
+    console.log(name);
+    var email=$("#email").val().trim()
+    console.log(email);
+    var message=$("#message").val().trim()
+    console.log(message);
+    database.ref().push({
+        name:name,
+        email:email,
+        message:message,
+    })
+  });
